6babc7c97462bd5e700435761d3a8c8f9dcd26b8
Jonatas Sas <atendimento@jsas.com.br>
2020-12-25 00:20:50 -0300

    Testing Initialization Script

--- tests/_config/testing.php
+++ tests/_config/testing.php
@@ -1,22 +1,38 @@
 <?php
 
 return [
-	'TEST_SITE_DB_DSN'         => 'mysql:host=localhost;dbname={DB}',
-	'TEST_SITE_DB_HOST'        => 'localhost',
-	'TEST_SITE_DB_NAME'        => '{DB}',
-	'TEST_SITE_DB_USER'        => 'root',
-	'TEST_SITE_DB_PASSWORD'    => 'root',
-	'TEST_SITE_TABLE_PREFIX'   => 'wp_',
-	'TEST_SITE_ADMIN_USERNAME' => 'admin',
-	'TEST_SITE_ADMIN_PASSWORD' => 'admin',
-	'TEST_SITE_WP_ADMIN_PATH'  => '/wp-admin',
-	'WP_ROOT_FOLDER'           => '/var/www/html',
-	'TEST_DB_NAME'             => '{DB}',
-	'TEST_DB_HOST'             => 'localhost',
-	'TEST_DB_USER'             => 'root',
-	'TEST_DB_PASSWORD'         => 'root',
-	'TEST_TABLE_PREFIX'        => 'wp_',
-	'TEST_SITE_WP_URL'         => 'http://localhost',
-	'TEST_SITE_WP_DOMAIN'      => 'localhost',
-	'TEST_SITE_ADMIN_EMAIL'    => 'admin@wordpress.test',
+	'TEST_SITE_DB_DSN'               => 'mysql:host=localhost;dbname={DB}',
+	'TEST_SITE_DB_HOST'              => 'localhost',
+	'TEST_SITE_DB_NAME'              => '{DB}',
+	'TEST_SITE_DB_USER'              => 'root',
+	'TEST_SITE_DB_PASSWORD'          => 'root',
+	'TEST_SITE_TABLE_PREFIX'         => 'wp_',
+
+	'TEST_DB_NAME'                   => '{DB}',
+	'TEST_DB_HOST'                   => 'localhost',
+	'TEST_DB_USER'                   => 'root',
+	'TEST_DB_PASSWORD'               => 'root',
+	'TEST_TABLE_PREFIX'              => 'wp_',
+
+	'TEST_SITE_ADMIN_USERNAME'       => 'admin',
+	'TEST_SITE_ADMIN_PASSWORD'       => 'admin',
+	'TEST_SITE_ADMIN_EMAIL'          => 'support@noxplugins.com',
+
+	'TEST_SITE_EDITOR_USERNAME'      => 'adam',
+	'TEST_SITE_EDITOR_EMAIL'         => 'adam@noxplugins.com',
+	'TEST_SITE_EDITOR_PASSWORD'      => 'editor',
+
+	'TEST_SITE_AUTHOR_USERNAME'      => 'clara',
+	'TEST_SITE_AUTHOR_EMAIL'         => 'clara@noxplugins.com',
+	'TEST_SITE_AUTHOR_PASSWORD'      => 'author',
+
+	'TEST_SITE_CONTRIBUTOR_USERNAME' => 'iris',
+	'TEST_SITE_CONTRIBUTOR_EMAIL'    => 'iris@noxplugins.com',
+	'TEST_SITE_CONTRIBUTOR_PASSWORD' => 'contributor',
+
+	'TEST_SITE_TITLE'                => 'NOX Plugin Test',
+	'TEST_SITE_WP_URL'               => 'http://localhost',
+	'TEST_SITE_WP_DOMAIN'            => 'localhost',
+	'WP_ROOT_FOLDER'                 => '/var/www/html',
+	'TEST_SITE_WP_ADMIN_PATH'        => '/wp-admin',
 ];
